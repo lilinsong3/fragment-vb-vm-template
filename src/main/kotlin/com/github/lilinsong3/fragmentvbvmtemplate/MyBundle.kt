3e58555bedc213eb6ad1b21a66497c587916b38c
GitHub Action <action@github.com>
2024-11-16 07:10:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilinsong3/fragmentvbvmtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/lilinsong3/fragmentvbvmtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lilinsong3.fragmentvbvmtemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lilinsong3/fragmentvbvmtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lilinsong3/fragmentvbvmtemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lilinsong3.fragmentvbvmtemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
